2c91a9ebb9fdd5663821b52405118b8673b107c1
opk00637-glitch <opk00637@gmail.com>
2026-02-03 22:59:13 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/plugins/tools/ping.py
@@ -0,0 +1,45 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from KRITIMUSIC import app
+from KRITIMUSIC.core.call import Miku
+from KRITIMUSIC.utils import bot_sys_stats
+from KRITIMUSIC.utils.decorators.language import language
+from KRITIMUSIC.utils.inline import supp_markup
+from KRITIMUSIC.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+
+
+@app.on_message(filters.command("ping", prefixes=["/", "!"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_video(
+        video="https://files.catbox.moe/51ocgi.mp4",
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Miku.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
