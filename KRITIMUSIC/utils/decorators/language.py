6ae4fb909f86686fac750563fcf8d9470f6fa346
opk00637-glitch <opk00637@gmail.com>
2026-02-03 23:00:24 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/utils/decorators/language.py
@@ -0,0 +1,72 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+from KRITIMUSIC.misc import SUDOERS
+from KRITIMUSIC.utils.database import get_lang, is_maintenance
+from strings import get_string
+
+
+def language(mystic):
+    async def wrapper(_, message, **kwargs):
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    disable_web_page_preview=True,
+                )
+        try:
+            await message.delete()
+        except:
+            pass
+
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
+
+
+def languageCB(mystic):
+    async def wrapper(_, CallbackQuery, **kwargs):
+        if await is_maintenance() is False:
+            if CallbackQuery.from_user.id not in SUDOERS:
+                return await CallbackQuery.answer(
+                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    show_alert=True,
+                )
+        try:
+            language = await get_lang(CallbackQuery.message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, CallbackQuery, language)
+
+    return wrapper
+
+
+def LanguageStart(mystic):
+    async def wrapper(_, message, **kwargs):
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
