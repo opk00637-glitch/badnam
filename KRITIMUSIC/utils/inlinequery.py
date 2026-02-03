058297e35841e36d0aa574cab90d66db8659718d
opk00637-glitch <opk00637@gmail.com>
2026-02-03 23:00:06 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/utils/inlinequery.py
@@ -0,0 +1,60 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="Pᴀᴜsᴇ",
+            description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="Rᴇsᴜᴍᴇ",
+            description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="Sᴋɪᴩ",
+            description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="Eɴᴅ",
+            description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("/end"),
+        ),
+        InlineQueryResultArticle(
+            title="Sʜᴜғғʟᴇ",
+            description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("/shuffle"),
+        ),
+        InlineQueryResultArticle(
+            title="Lᴏᴏᴩ",
+            description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
