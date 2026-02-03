4ea832716e9483f41575cdb5a5c4cf6171e16558
opk00637-glitch <opk00637@gmail.com>
2026-02-03 22:58:12 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/plugins/admins/stop.py
@@ -0,0 +1,39 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from KRITIMUSIC import app
+from KRITIMUSIC.core.call import Miku
+from KRITIMUSIC.utils.database import set_loop
+from KRITIMUSIC.utils.decorators import AdminRightsCheck
+from KRITIMUSIC.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Miku.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
