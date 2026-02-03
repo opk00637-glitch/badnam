f41449a019747b757cfda3e2a22c8554353b9bc4
opk00637-glitch <opk00637@gmail.com>
2026-02-03 22:59:15 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/plugins/sudo/logger.py
@@ -0,0 +1,46 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+from pyrogram import filters
+
+from KRITIMUSIC import app
+from KRITIMUSIC.misc import SUDOERS
+from KRITIMUSIC.utils.database import add_off, add_on
+from KRITIMUSIC.utils.decorators.language import language
+
+
+@app.on_message(filters.command(["logger"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state == "enable":
+        await add_on(2)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(2)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
+
+
+@app.on_message(filters.command(["cookies"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    await message.reply_document("cookies/logs.csv")
+    await message.reply_text("Please check given file to cookies file choosing logs...")
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
