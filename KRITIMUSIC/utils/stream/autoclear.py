76c2a4421fc97cd0a42db8f7423dbea290fa8ba2
opk00637-glitch <opk00637@gmail.com>
2026-02-03 23:00:40 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/utils/stream/autoclear.py
@@ -0,0 +1,34 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+import os
+
+from config import autoclean
+
+
+async def auto_clean(popped):
+    try:
+        rem = popped["file"]
+        autoclean.remove(rem)
+        count = autoclean.count(rem)
+        if count == 0:
+            if "vid_" not in rem or "live_" not in rem or "index_" not in rem:
+                try:
+                    os.remove(rem)
+                except:
+                    pass
+    except:
+        pass
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
