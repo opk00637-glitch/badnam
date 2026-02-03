4970c6958e9dbe75b62cd7290ff58c07f82460f3
opk00637-glitch <opk00637@gmail.com>
2026-02-03 23:00:28 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/utils/inline/stats.py
@@ -0,0 +1,66 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def stats_buttons(_, status):
+    not_sudo = [
+        InlineKeyboardButton(
+            text=_["SA_B_1"],
+            callback_data="TopOverall",
+        )
+    ]
+    sudo = [
+        InlineKeyboardButton(
+            text=_["SA_B_2"],
+            callback_data="bot_stats_sudo",
+        ),
+        InlineKeyboardButton(
+            text=_["SA_B_3"],
+            callback_data="TopOverall",
+        ),
+    ]
+    upl = InlineKeyboardMarkup(
+        [
+            sudo if status else not_sudo,
+            [
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ],
+        ]
+    )
+    return upl
+
+
+def back_stats_buttons(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data="stats_back",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ],
+        ]
+    )
+    return upl
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
