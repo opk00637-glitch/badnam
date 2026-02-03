6078bcd143628cbe1a0518b62b6a7f5823d85fc1
opk00637-glitch <opk00637@gmail.com>
2026-02-03 22:58:11 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KRITIMUSIC/plugins/admins/purge.py
@@ -0,0 +1,104 @@
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
+
+from asyncio import sleep
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.errors import MessageDeleteForbidden, RPCError
+from pyrogram.types import Message
+from KRITIMUSIC.utils.Miku_BAN import admin_filter
+from KRITIMUSIC import app
+
+
+@app.on_message(filters.command("purge") & admin_filter)
+async def purge(app: app, msg: Message):
+    
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**⋟ ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+        return
+
+    if msg.reply_to_message:
+        message_ids = list(range(msg.reply_to_message.id, msg.id))
+
+        def divide_chunks(l: list, n: int = 100):
+            for i in range(0, len(l), n):
+                yield l[i : i + n]
+
+        m_list = list(divide_chunks(message_ids))
+
+        try:
+            for plist in m_list:
+                await app.delete_messages(chat_id=msg.chat.id, message_ids=plist, revoke=True)
+                
+            await msg.delete()
+        except MessageDeleteForbidden:
+            await msg.reply_text(text="**⋟ ɪ ᴄᴀɴ'ᴛ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ɴᴏᴛ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ.**")
+            return
+            
+        except RPCError as ef:
+            await msg.reply_text(text=f"**⋟ sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ, ʀᴇᴘᴏʀᴛ ɪᴛ ᴜsɪɴɢ** `/bug` **ᴇʀʀᴏʀ :-** `{ef}`")
+        
+        count_del_msg = len(message_ids)
+        sumit = await msg.reply_text(text=f"**⋟ ᴅᴇʟᴇᴛᴇᴅ** <i>{count_del_msg}</i> **ᴍᴇssᴀɢᴇs**")
+        await sleep(3)
+        await sumit.delete()
+        return
+    await msg.reply_text("**⋟ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢᴇ.**")
+    return
+
+
+@app.on_message(filters.command("spurge") & admin_filter)
+async def spurge(app: app, msg: Message):
+
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**⋟ ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+        return
+
+    if msg.reply_to_message:
+        message_ids = list(range(msg.reply_to_message.id, msg.id))
+
+        def divide_chunks(l: list, n: int = 100):
+            for i in range(0, len(l), n):
+                yield l[i : i + n]
+
+        m_list = list(divide_chunks(message_ids))
+
+        try:
+            for plist in m_list:
+                await app.delete_messages(chat_id=msg.chat.id, message_ids=plist, revoke=True)
+            await msg.delete()
+        except MessageDeleteForbidden:
+            await msg.reply_text(text="**⋟ ɪ ᴄᴀɴ'ᴛ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ɴᴏᴛ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ.**")
+            return
+            
+        except RPCError as ef:
+            await msg.reply_text(text=f"**⋟ sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ, ʀᴇᴘᴏʀᴛ ɪᴛ ᴜsɪɴɢ** `/bug` **ᴇʀʀᴏʀ :-** <code>{ef}</code>")           
+            return        
+    await msg.reply_text("**⋟ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢᴇ.**")
+    return
+
+
+@app.on_message(filters.command("del") & admin_filter)
+async def del_msg(app: app, msg: Message):
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**⋟ ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+        return        
+    if msg.reply_to_message:
+        await msg.delete()
+        await app.delete_messages(chat_id=msg.chat.id, message_ids=msg.reply_to_message.id)
+    else:
+        await msg.reply_text(text="**⋟ ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ.**")
+        return
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by Kirti 😎
+
+# 🧑‍💻 Developer : t.me/lll_APNA_BADNAM_BABY_lll
+# 🔗 Source link : https://github.com/Badnam019
+# 📢 Telegram channel : t.me/lll_APNA_BADNAM_BABY_lll
+# =======================================================
